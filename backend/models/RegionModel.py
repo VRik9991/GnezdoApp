2456540fed484e4f824c15982c09ae9ad8758c0b
alexe <alexey.perepel@gmail.com>
2026-03-16 21:43:00 +0300

    have fun 2

---
+++ backend/models/RegionModel.py
@@ -0,0 +1,32 @@
+from enum import Enum
+from beanie import Document, Link
+
+from backend.models import UserModel
+
+
+class RegionStatus(Enum):
+    ABUNDANT = 'Abundant'
+    CALM = 'Calm'
+    TENSE = 'Tense',
+    EXCITED = 'Excited',
+    CONFLICT = 'Conflict',
+    WAR = 'War'
+
+class RegionConditions(Enum):
+    HUNTERS = 'Охотники'
+    WEREWOLVES = 'Оборотни'
+    SPIRITS = 'Духи'
+    FEY = 'Феи'
+    WIZARDS = 'Маги'
+    DEMONS = 'Демоны'
+
+class RegionModel(Document):
+    name: str
+    owner: Link[UserModel] | None
+    status: RegionStatus
+    resource: int
+    conditions: list[RegionConditions]
+
+
+    class Settings:
+        name = "regions"
